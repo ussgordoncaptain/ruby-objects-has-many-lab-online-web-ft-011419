41763dea79c02aab4f993b5d7d9c604077b4f2b6
Edmund Nelson <edmund.nelson.b@gmail.com>
2019-02-01 11:06:11 +0000

    Automatically backed up by Learn

--- lib/author.rb
+++ lib/author.rb
@@ -1,7 +1,16 @@
 class Author 
+  @@posts_count = 0
   def initialize(name)
     @name = name
+    @posts = []
   end
   attr_accessor :name
-  
+  def posts
+    @posts
+  end
+  def add_post(post)
+    @posts << post 
+    @@posts_count +=1
+  end
+  def add_post_by_title(title)
 end 
